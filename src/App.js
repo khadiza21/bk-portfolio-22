16cdeac5a3d679067b4e9744a43c24c10a672ead
khadiza21 <bibikhadiza474@gmail.com>
2022-08-03 07:18:33 +0600

    updating portfolio

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import { Route, Routes } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 import './App.css';
-import Home from './components/Home/Home';
-import Footer from './components/Shared/Footer/Footer';
-import Header from './components/Shared/Header/Header';
+
 
 
 function App() {
   return (
     <div>
-      <Header></Header>
+     
       <Routes>
-        <Route path="/" element={<Home></Home>}></Route>
+        {/* <Route path="/" element={<Home></Home>}></Route> */}
       
       </Routes>
-      <Footer></Footer>
+    
       <ToastContainer></ToastContainer>
     </div>
   );
