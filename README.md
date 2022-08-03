56de4cf236f506dd07010ff7b4966cd5438d0dad
khadiza21 <bibikhadiza474@gmail.com>
2022-08-03 07:04:43 +0600

    updating portfolio

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
-
-<!-- June 04
--------- -->
-<!-- Task: 15 connection build & 45 apply (BD - 10, India - 15, Inernshala -15 , USA -15) Time ::: Today morning 10am - night 10am -->
+resume : https://www.freepik.com/free-photos-vectors/portfolio-template?format=collections
+blog: https://iishanto.com/
--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import './App.css';
 import Home from './components/Home/Home';
 import Footer from './components/Shared/Footer/Footer';
 import Header from './components/Shared/Header/Header';
-import Projects from './components/Home/Projects/Projects/Projects';
 
 
 function App() {
@@ -14,15 +13,7 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/home" element={<Home></Home>}></Route>
-        <Route path="/projects" element={<Projects></Projects>}></Route>
-       
-        {/* <Route path="/reviews" element={<Reviews></Reviews>}></Route>
-        <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
-        <Route path="/blogs" element={<Blogs></Blogs>}></Route>
-        <Route path="/about" element={<About></About>}></Route>
-        <Route path="/contact" element={<Contact></Contact>}></Route>
-        <Route path="*" element={<Notfound></Notfound>}></Route> */}
+      
       </Routes>
       <Footer></Footer>
       <ToastContainer></ToastContainer>
